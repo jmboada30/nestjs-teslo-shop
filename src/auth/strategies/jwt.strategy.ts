fade48d3983a8c8323ff6075767e02ec483f668c
Joel M. Boada <jmboada30@gmail.com>
2022-07-29 05:49:07 -0400

    init jwt module

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private consfigService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: consfigService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { email } = payload;
+
+    const user = await this.userRepository.findOneBy({ email });
+
+    if (!user) throw new UnauthorizedException('Creditentials invalid');
+
+    if (!user.isActive) throw new UnauthorizedException('User is not active');
+
+    return user;
+  }
+}
